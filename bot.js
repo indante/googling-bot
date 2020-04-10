fec141c3558734e9281d09aa4b082fc72ca3853a
iam-dante <koreanhoseon@gmail.com>
2020-04-10 22:23:58 +0900

    공백 치환

--- bot.js
+++ bot.js
@@ -6,10 +6,12 @@ const rtm = new RTMClient(token);
 rtm.start();
 
 rtm.on('message', (message) => {
-    var text = message.text
-    var googling_keyword = text.slice(4);
-    var call_sign = text.slice(0,3);
+    var text = message.text // 슬랙 전체 메세지
+    var googling_keyword = text.slice(4); // 검색할 키워드
+    var googling_detail_keyword = googling_keyword.replace(' ',"+") // 공백을 + 로 치환
+    var call_sign = text.slice(0,3); // !구글
+    console.log(googling_detail_keyword);
     if(call_sign==="!구글"){
-        rtm.sendMessage(`google.com/search?q=${googling_keyword}`, message.channel)
+        rtm.sendMessage(`https://www.google.com/search?q=${googling_detail_keyword}`, message.channel)
     }
 });
