b99a9ee73daee4262cfba4044df67869b7f55449
iam-dante <koreanhoseon@gmail.com>
2020-04-12 20:54:29 +0900

    공백 하나만 처리해주는거 해결

--- bot.js
+++ bot.js
@@ -8,10 +8,10 @@ rtm.start();
 rtm.on('message', (message) => {
     var text = message.text // 슬랙 전체 메세지
     var korean_googling_keyword = text.slice(4); // 검색할 키워드
-    var korean_googling_detail_keyword = korean_googling_keyword.replace(' ', "+"); // 공백을 + 로 치환
+    var korean_googling_detail_keyword = korean_googling_keyword.replace(/ /gi, "+"); // 공백을 + 로 치환
     var korean_call_sign = text.slice(0,3); // !구글
     var english_googling_keyword = text.slice(8);
-    var english_googling_detail_keyword = english_googling_keyword.replace(' ', "+");
+    var english_googling_detail_keyword = english_googling_keyword.replace(/ /gi, "+");
     var english_call_sign = text.slice(0,7)
     if(korean_call_sign==="!구글"){
         rtm.sendMessage(`https://www.google.com/search?q=${korean_googling_detail_keyword}`, message.channel)
