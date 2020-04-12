2c613915bead647a3f33e884c1ec6c14e9b9efc4
iam-dante <koreanhoseon@gmail.com>
2020-04-12 20:42:43 +0900

    한글 외 영어 명령어도 추가

--- bot.js
+++ bot.js
@@ -7,11 +7,15 @@ rtm.start();
 
 rtm.on('message', (message) => {
     var text = message.text // 슬랙 전체 메세지
-    var googling_keyword = text.slice(4); // 검색할 키워드
-    var googling_detail_keyword = googling_keyword.replace(' ',"+") // 공백을 + 로 치환
-    var call_sign = text.slice(0,3); // !구글
-    console.log(googling_detail_keyword);
-    if(call_sign==="!구글"){
-        rtm.sendMessage(`https://www.google.com/search?q=${googling_detail_keyword}`, message.channel)
+    var korean_googling_keyword = text.slice(4); // 검색할 키워드
+    var korean_googling_detail_keyword = korean_googling_keyword.replace(' ', "+"); // 공백을 + 로 치환
+    var korean_call_sign = text.slice(0,3); // !구글
+    var english_googling_keyword = text.slice(8);
+    var english_googling_detail_keyword = english_googling_keyword.replace(' ', "+");
+    var english_call_sign = text.slice(0,7)
+    if(korean_call_sign==="!구글"){
+        rtm.sendMessage(`https://www.google.com/search?q=${korean_googling_detail_keyword}`, message.channel)
+    } else if(english_call_sign==="!google"||"!Google"){
+        rtm.sendMessage(`https://www.google.com/search?q=${english_googling_detail_keyword}`, message.channel)
     }
 });
